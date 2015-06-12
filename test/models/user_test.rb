138b73a44749e41f8a10ba5b56ba53b19a4a4edf
Deutro <patrick.engelkes@gmail.com>
2015-06-12 18:16:01 +0200

    Log in and Log out.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,8 @@ class UserTest < ActiveSupport::TestCase
     @user.reload
     assert_equal 'foo@bar.com', @user.email
   end
+
+  test 'authenticated? should return false for a user with nil digest' do
+    assert_not @user.authenticated?(' ')
+  end
 end
