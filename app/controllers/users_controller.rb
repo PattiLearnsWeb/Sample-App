889ac304eb43ef85f439080460880eb6861a25bb
Deutro <patrick.engelkes@gmail.com>
2015-06-15 18:40:50 +0200

    Finish user edit, update, index adn destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -18,6 +27,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    set_user
+  end 
+
+  def update
+    set_user
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_url
+  end
+
   private
   def set_user
     @user = User.find(params[:id])
@@ -25,6 +54,23 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    set_user
+    redirect_to(root_url) unless current_user(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
